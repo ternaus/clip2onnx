7c32bca7554ab6de1feb1659b4d1b3c80825f284
Vladimir Iglovikov <iglovikov@gmail.com>
2023-01-02 18:13:13 -0800

    Added model for multilingual conversion

--- model_conversion/textual.py
+++ model_conversion/textual.py
@@ -1,18 +1,18 @@
-"""Code from https://github.com/Lednik7/CLIP-ONNX"""
-from torch import nn
+"""Code from https://github.com/Lednik7/CLIP-ONNX."""
 import torch
+from torch import nn
+
 
 class Textual(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model: nn.Module) -> None:
         super().__init__()
         self.transformer = model.transformer
         self.positional_embedding = model.positional_embedding
-        self.transformer = model.transformer
         self.ln_final = model.ln_final
         self.text_projection = model.text_projection
         self.token_embedding = model.token_embedding
 
-    def forward(self, text):
+    def forward(self, text: torch.Tensor) -> torch.Tensor:
         x = self.token_embedding(text)  # [batch_size, n_ctx, d_model]
 
         x += self.positional_embedding
@@ -21,7 +21,6 @@ class Textual(nn.Module):
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.ln_final(x)
 
-        # x.shape = [batch_size, n_ctx, transformer.width]
         # take features from the eot embedding (eot_token is the highest number in each sequence)
         # needs .float() before .argmax(  ) to work
-        return x[torch.arange(x.shape[0]), text.float().argmax(dim=-1)] @ self.text_projection 
+        return x[torch.arange(x.shape[0]), text.float().argmax(dim=-1)] @ self.text_projection
